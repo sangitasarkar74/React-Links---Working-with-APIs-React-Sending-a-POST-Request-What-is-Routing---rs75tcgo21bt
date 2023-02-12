88ad075795de0f89f893d2eb7c179d8f9f601f96
sangitasarkar74 <sangitasarkar@gmail.com>
2023-02-12 21:47:18 +0530

    changes updated

--- src/components/RenderingTopic.js
+++ src/components/RenderingTopic.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 const RenderingTopic = () => {
+  const history = useHistory();
   return (
     <div id="rendering-div">
       <h3>Rendering in React</h3>
-      <button id="rendering-back-button">
+      <button
+        id="rendering-back-button"
+        onClick={() => history.push("/topics")}
+      >
         Back
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default RenderingTopic
+export default RenderingTopic;
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
 const Topics = () => {
+  const history = useHistory();
+
   return (
     <div id="topics-div">
       <h2>Topics</h2>
       <ul>
-        <li>
-          <h2 id="rendering">Rendering</h2>
-        </li>
-        <li>
-          <h2 id="components">Components</h2>
-        </li>
+        <Link to="/topics/rendering">
+          <li>
+            <h2 id="rendering">Rendering</h2>
+          </li>
+        </Link>
+        <Link to="/topics/components">
+          <li>
+            <h2 id="components">Components</h2>
+          </li>
+        </Link>
       </ul>
       <hr />
-      <button id="topics-back-button">Back</button>
+      <button id="topics-back-button" onClick={() => history.push("/")}>
+        Back
+      </button>
     </div>
   );
 };
